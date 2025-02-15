8b3c771d52b63c02b91e86ade7d76cc8afecb160
MuTe43 <mute_wajdi@hotmail.fr>
2025-02-15 11:53:00 +0100

    Add demo module

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'estate',
+    'depends': [
+        'base'
+    ],
+    'data':[
+        'security/ir.model.access.csv'
+    ],
+    'application':True,
+}
