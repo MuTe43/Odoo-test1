ae9aa3daa0e2a17936316d41b312e5039fbb18f5
MuTe43 <mute_wajdi@hotmail.fr>
2025-02-17 03:52:31 +0100

    Halfway chap6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -9,14 +9,14 @@ class EstateProperty(models.Model):
     _description = "estate desc"
 
 
-    name = fields.Char('Plan Name', required=True, translate=True)
+    name = fields.Char('Name', required=True, translate=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(default=Datetime.today()+timedelta(30*3),copy=False)
+    date_availability = fields.Date('Available From',default=Datetime.today()+timedelta(30*3),copy=False)
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
